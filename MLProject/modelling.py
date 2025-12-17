0d2d994fd6c59d123a014d2aa975a4aa96159a09
yudistirta2294 <yudistirtabp@gmail.com>
2025-12-17 16:04:47 +0700

    Basic Workflow-CI pipeline

---
+++ MLProject/modelling.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+mlflow.set_experiment("Exam Score Modelling")
+
+data = pd.read_csv("exam_score_preprocessed.csv")
+
+X = data.drop("exam_score", axis=1)
+y = data["exam_score"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+mlflow.sklearn.autolog()
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("MSE:", mse)
+print("R2:", r2)
